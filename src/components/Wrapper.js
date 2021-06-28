cdc75495b5f6febd687e260ee12c0414acbbd33f
Matt Clark <clark.matthewr@gmail.com>
2021-06-27 22:16:41 -0500

    fix build issue

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -43,7 +43,12 @@ const Main = styled.main`
   }
 `;
 
-const capitalize = (text) => text[0].toUpperCase() + text.slice(1);
+const capitalize = (text) => {
+  if (text) {
+    return text[0].toUpperCase() + text.slice(1);
+  }
+  return text;
+};
 
 export default function Wrapper({ children }) {
   const router = useRouter();
