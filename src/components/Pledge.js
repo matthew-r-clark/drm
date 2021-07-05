e3c31dcf80853cd20ce48e84b0c118ce2c94f843
Jon Smith <aardvarkjon94@yahoo.com>
2021-07-05 17:07:58 -0500

    Linter fixes

--- src/components/Pledge.js
+++ src/components/Pledge.js
@@ -1,16 +1,16 @@
 import styled from '@emotion/styled';
-import { DesktopOnly } from './MediaQuery';
-const Pledge = ({instance}) => {
-    const Grid = styled.div`
+
+const Pledge = ({ instance }) => {
+  const Grid = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
     margin-left: 1rem;
     `;
-    return ( 
-        <Grid>
-            <h4>{instance.first_name}</h4>
-            <h4>{instance.last_name}</h4>
-        </Grid>
+  return (
+      <Grid>
+          <h4>{instance.first_name}</h4>
+          <h4>{instance.last_name}</h4>
+      </Grid>
        
      );
 }
