4542c08486e7830b6a160b1296a773576180c43b
Matt Clark <clark.matthewr@gmail.com>
2021-07-12 22:22:02 -0500

    add get route for ministers

---
+++ src/pages/api/ministers/index.js
@@ -0,0 +1,23 @@
+import router from '@@router';
+import supabase from '@@supabase';
+
+const api = {
+  get: async (req, res) => {
+    const { data, error } = await supabase
+    .from('staff')
+    .select('*');
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(400).send(error);
+    }
+  },
+
+  put: (req, res) => {},
+
+  post: (req, res) => {},
+
+  delete: (req, res) => {},
+};
+
+export default router(api);
