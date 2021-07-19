6894b2c9e8ddffe7ca30fb892d08c85e8826c051
Matt Clark <clark.matthewr@gmail.com>
2021-07-18 19:00:50 -0500

    add pagination control for ministers

--- src/pages/api/ministers/index.js
+++ src/pages/api/ministers/index.js
@@ -1,19 +1,37 @@
 import router from '@@router';
 import supabase from '@@supabase';
-import { isEmpty } from 'ramda';
+import { isEmpty, omit } from 'ramda';
+
+const ROW_LIMIT = 15;
 
 const api = {
   get: async (req, res) => {
-    const { data, error } = await supabase
+    let { limit = ROW_LIMIT, page = 1 } = req.query;
+    limit = Number(limit);
+    page = Number(page);
+    const query = omit(['limit', 'page'], req.query);
+    const startRange = (page - 1) * limit;
+    const endRange = startRange + limit - 1;
+    const { data, error, count } = await supabase
       .from('ministers')
-      .select()
-      .match(req.query);
+      .select('*', { count: 'exact' })
+      .match(query)
+      .range(startRange, endRange);
     if (data && isEmpty(data)) {
-      res.status(404).send(`No ministers found matching query:\n${JSON.stringify(req.query)}`);
+      res.status(404).send(`No ministers found matching query:\n${JSON.stringify(query)}`);
     } else if (error) {
       res.status(400).send(error);
     } else {
-      res.status(200).json(data);
+      const pages = Math.ceil(count / limit);
+      const payload = {
+        limit,
+        page,
+        pages,
+        page_count: data.length,
+        total_count: count,
+        data,
+      };
+      res.status(200).json(payload);
     }
   },
 
