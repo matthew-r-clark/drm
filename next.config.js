d8fde8fd4a172abe0d0d3028fb6d9482f9aeee46
Matt Clark <clark.matthewr@gmail.com>
2021-08-22 22:19:53 -0500

    use public config for both

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ module.exports = {
       url: process.env.SUPABASE_URL,
       key: process.env.SUPABASE_KEY,
     },
-    logtailSourceToken: process.env.LOGTAIL_SOURCE_TOKEN,
   },
   publicRuntimeConfig: {
     apiAuthKey: process.env.NEXT_PUBLIC_API_AUTH_KEY,
