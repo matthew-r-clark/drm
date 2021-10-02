e9f23b60b72829d1c5e7d79e92be5c554b1b0906
Matt Clark <clark.matthewr@gmail.com>
2021-10-02 14:37:15 -0500

    remove unecessary imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styled from '@emotion/styled';
 import { Button, Snackbar } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
