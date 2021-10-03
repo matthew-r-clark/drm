8ea8243db6d283b72c44b61c41aa2d693f653468
Matt Clark <clark.matthewr@gmail.com>
2021-10-03 17:53:26 -0500

    remove commented pagination, return empty array if no results, remove pagination from ministers

--- src/pages/api/ministers/index.js
+++ src/pages/api/ministers/index.js
@@ -31,32 +31,13 @@ const api = {
   }
   */
   get: async (req, res) => {
-    let { limit = 15, page = 1 } = req.query;
-    limit = Number(limit);
-    page = Number(page);
-    const query = omit(['limit', 'page'], req.query);
-    const startRange = (page - 1) * limit;
-    const endRange = startRange + limit - 1;
-    const { data, error, count } = await supabase
+    const { data, error } = await supabase
       .from('ministers')
-      .select('*', { count: 'exact' })
-      .match(query)
-      .range(startRange, endRange);
-    if (data && isEmpty(data)) {
-      res.status(404).send(`No ministers found matching query:\n${JSON.stringify(query)}`);
-    } else if (error) {
+      .select();
+    if (error) {
       res.status(400).send(error);
     } else {
-      const pages = Math.ceil(count / limit);
-      const payload = {
-        limit,
-        page,
-        pages,
-        page_count: data.length,
-        total_count: count,
-        data,
-      };
-      res.status(200).json(payload);
+      res.status(200).json(data);
     }
   },
 
