e5d92df1ba5ccc23dfd56146e6de6caa4bfc07e2
Matt Clark <clark.matthewr@gmail.com>
2021-10-03 16:21:16 -0500

    return empty array if no results found

--- src/pages/api/connections/pledges/[ministerId].js
+++ src/pages/api/connections/pledges/[ministerId].js
@@ -47,9 +47,7 @@ const api = {
       .from('pledges')
       .select()
       .match({ minister_id: ministerId });
-    if (data && isEmpty(data)) {
-      res.status(404).send(`No pledges found for minister with ID ${ministerId}.`);
-    } else if (error) {
+    if (error) {
       res.status(400).send(error);
     } else {
       res.status(200).json(data);
--- src/pages/api/connections/prospects/[ministerId].js
+++ src/pages/api/connections/prospects/[ministerId].js
@@ -46,9 +46,7 @@ const api = {
       .from('prospects') // sql view
       .select()
       .match({ minister_id: ministerId });
-    if (data && isEmpty(data)) {
-      res.status(404).send(`No prospects found for minister with ID ${ministerId}.`);
-    } else if (error) {
+    if (error) {
       res.status(400).send(error);
     } else {
       res.status(200).json(data);
