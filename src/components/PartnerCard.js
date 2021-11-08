a4148684deb08da5ea2563a6ee5c77b7ab06c92c
Matt Clark <clark.matthewr@gmail.com>
2021-11-07 21:41:50 -0600

    use material ui input components

--- src/components/PartnerCard.js
+++ src/components/PartnerCard.js
@@ -1,4 +1,6 @@
-import { Button, Grid } from '@material-ui/core';
+import {
+  Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField,
+} from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import styled from '@emotion/styled';
 import { useState } from 'react';
@@ -8,7 +10,7 @@ import {
 } from 'formik';
 import {
   clone,
-  find, map, path, pipe, propEq, tail,
+  find, last, map, path, pipe, propEq, tail,
 } from 'ramda';
 
 import { deletePartnerById, updatePartnerById } from 'modules/partners';
@@ -64,6 +66,38 @@ const setPrimaryAlias = (aliases, index) => {
 
 const formatNamesList = map((name) => <>{name}<br /></>);
 
+const onBirthdateChange = (e) => {
+  if (!/[\d]/.test(e.target.value)) {
+    e.target.value = '';
+    return e;
+  }
+  const birthdate = e.target.value
+    .replace(/[\D+]/g, '')
+    .substring(0, 4);
+  const convert = (match, month = '', day = '') => {
+    if (Number(month[0]) > 1) {
+      return `0${month}/`;
+    }
+    if (Number(month) > 12 || month.length === 1) {
+      return month[0];
+    }
+    const maxDaysInMonth = new Date(2020, month, 0).getDate();
+    const maxDaysInMonthFirstDigit = Number(String(maxDaysInMonth)[0]);
+    if (Number(day[0]) > maxDaysInMonthFirstDigit) {
+      return `${month}/0${day}`;
+    }
+    if (Number(day) > maxDaysInMonth || day.length === 1) {
+      return `${month}/${day[0]}`;
+    }
+    return `${month}/${day}`;
+  };
+  e.target.value = birthdate.replace(
+    /([0-9]{1,2})([0-9]{1,2})?/,
+    convert,
+  );
+  return e;
+};
+
 export default function PartnerCard({ id, isOpen, close }) {
   const [isEditing, setIsEditing] = useState(false);
   const dispatch = useDispatch();
@@ -75,8 +109,8 @@ export default function PartnerCard({ id, isOpen, close }) {
   const handleSubmit = (values) => {
     setIsEditing(false);
     const payload = clone(values);
-    payload.aliases = setPrimaryAlias(payload.aliases, payload.primaryName);
-    delete payload.primaryName;
+    payload.aliases = setPrimaryAlias(payload.aliases, payload.primaryNameIndex);
+    delete payload.primaryNameIndex;
     updatePartnerById(payload.id, payload);
     dispatch(updatePartnerInState(payload));
   };
@@ -98,41 +132,145 @@ export default function PartnerCard({ id, isOpen, close }) {
         <Formik
           initialValues={{
             ...partner,
-            primaryName: 0,
+            primaryNameIndex: 0,
           }}
           onSubmit={handleSubmit}
         >
-          {({ values }) => (
+          {({
+            values, handleChange, touched, errors, setFieldValue,
+          }) => (
             <Form>
               <Grid container direction="row" spacing={10}>
                 <GridItem xs={6}>
-                  <H3>Email</H3>
-                  <Field type="text" name="email" />
+                  <H3>Contact</H3>
+                  <Grid container direction="column">
+                    <TextField
+                      autoFocus
+                      id="email"
+                      name="email"
+                      label="Email"
+                      value={values.email}
+                      onChange={handleChange}
+                      error={touched.email && Boolean(errors.email)}
+                      helperText={touched.email && errors.email}
+                    />
 
-                  <H3>Phone</H3>
-                  <Field type="phone" name="phone" />
+                    <TextField
+                      id="phone"
+                      name="phone"
+                      label="Phone"
+                      value={values.phone}
+                      onChange={handleChange}
+                      error={touched.phone && Boolean(errors.phone)}
+                      helperText={touched.phone && errors.phone}
+                    />
 
-                  <H3>Preferred Contact</H3>
-                  <Field as="select" name="preferred_contact_method">
-                    <option value="phone">phone</option>
-                    <option value="text">text</option>
-                    <option value="email">email</option>
-                  </Field>
+                    <FormControl>
+                      <InputLabel id="preferred_contact_method">Preferred Contact</InputLabel>
+                      <Select
+                        id="preferred_contact_method"
+                        name="preferred_contact_method"
+                        labelId="preferred_contact_method"
+                        value={values.preferred_contact_method}
+                        onChange={handleChange}
+                        error={touched.preferred_contact_method
+                          && Boolean(errors.preferred_contact_method)}
+                        helperText={touched.preferred_contact_method
+                          && errors.preferred_contact_method}
+                      >
+                        <MenuItem value="">Select...</MenuItem>
+                        <MenuItem value="phone">phone</MenuItem>
+                        <MenuItem value="text">text</MenuItem>
+                        <MenuItem value="email">email</MenuItem>
+                      </Select>
+                    </FormControl>
 
-                  <H3>Address</H3>
-                  <Field type="text" name="address_line_1" placeholder="Street Address" />
-                  <Field type="text" name="address_line_2" placeholder="Suite/Apt Number" />
-                  <Field type="text" name="city" placeholder="city" />
-                  <Field type="text" name="state" placeholder="state" />
-                  <Field type="text" name="zip_code" placeholder="zip_code" />
+                    <div>
+                      <H3>Address</H3>
+                      <TextField
+                        fullWidth
+                        id="address_line_1"
+                        name="address_line_1"
+                        label="Street Address"
+                        value={values.address_line_1}
+                        onChange={handleChange}
+                        error={touched.address_line_1 && Boolean(errors.address_line_1)}
+                        helperText={touched.address_line_1 && errors.address_line_1}
+                      />
+                      <TextField
+                        fullWidth
+                        id="address_line_2"
+                        name="address_line_2"
+                        label="Suite/Apt Number"
+                        value={values.address_line_2}
+                        onChange={handleChange}
+                        error={touched.address_line_2 && Boolean(errors.address_line_2)}
+                        helperText={touched.address_line_2 && errors.address_line_2}
+                      />
+                      <TextField
+                        fullWidth
+                        id="city"
+                        name="city"
+                        label="City"
+                        value={values.city}
+                        onChange={handleChange}
+                        error={touched.city && Boolean(errors.city)}
+                        helperText={touched.city && errors.city}
+                      />
+                      <Grid container direction="row" justifyContent="space-between">
+                        <TextField
+                          id="state"
+                          name="state"
+                          label="State"
+                          value={values.state}
+                          onChange={handleChange}
+                          error={touched.state && Boolean(errors.state)}
+                          helperText={touched.state && errors.state}
+                          style={{ width: '45%' }}
+                        />
+                        <TextField
+                          id="zip_code"
+                          name="zip_code"
+                          label="Zip Code"
+                          value={values.zip_code}
+                          onChange={handleChange}
+                          error={touched.zip_code && Boolean(errors.zip_code)}
+                          helperText={touched.zip_code && errors.zip_code}
+                          style={{ width: '45%' }}
+                        />
+                      </Grid>
+                    </div>
+                  </Grid>
                 </GridItem>
 
                 <GridItem xs={6}>
-                  <H3>Birthday</H3>
-                  <Field type="text" name="birthday" />
+                  <H3>Personal</H3>
+                  <TextField
+                    id="birthday"
+                    name="birthday"
+                    label="Birthday (mm/dd)"
+                    value={values.birthday}
+                    onKeyDown={(e) => {
+                      if (e.key === 'Backspace' && last(values.birthday) === '/') {
+                        setFieldValue('birthday', values.birthday.slice(0, 2));
+                      }
+                    }}
+                    onChange={(e) => {
+                      handleChange(onBirthdateChange(e));
+                    }}
+                    error={touched.birthday && Boolean(errors.birthday)}
+                    helperText={touched.birthday && errors.birthday}
+                  />
 
-                  <H3>Spouse</H3>
-                  {partner.spouse || 'n/a'}
+                  <TextField
+                    id="spouse"
+                    name="spouse"
+                    label="Spouse"
+                    value={values.spouse}
+                    onChange={handleChange}
+                    error={touched.spouse && Boolean(errors.spouse)}
+                    helperText={touched.spouse && errors.spouse}
+                  />
 
                   <H3>Add/Remove Names</H3>
                   <FieldArray
@@ -141,8 +279,12 @@ export default function PartnerCard({ id, isOpen, close }) {
                       <>
                         {values.aliases.map((alias, index) => (
                           <Grid container direction="row">
-                            <Field name={`aliases.${index}`} style={{ width: '85%' }} />
-                            {index > 0
+                            <TextField
+                              name={`aliases.${index}`}
+                              value={values.aliases[index]}
+                              onChange={handleChange}
+                            />
+                            {values.aliases.length > 1
                               && <CloseButton onClick={() => arrayHelpers.remove(index)} />}
                           </Grid>
                         ))}
@@ -158,17 +300,24 @@ export default function PartnerCard({ id, isOpen, close }) {
                     )}
                   />
 
-                  <H3>Primary Name</H3>
-                  <Field
-                    as="select"
-                    name="primaryName"
-                  >
-                    {values.aliases.map((alias, index) => (
-                      <option key={alias} value={index}>
-                        {alias}
-                      </option>
-                    ))}
-                  </Field>
+                  <FormControl>
+                    <InputLabel id="primaryNameIndex">Primary Name</InputLabel>
+                    <Select
+                      id="primaryNameIndex"
+                      name="primaryNameIndex"
+                      labelId="primaryNameIndex"
+                      value={values.primaryNameIndex}
+                      onChange={handleChange}
+                      error={touched.primaryNameIndex && Boolean(errors.primaryNameIndex)}
+                      helperText={touched.primaryNameIndex && errors.primaryNameIndex}
+                    >
+                      {values.aliases.map((alias, index) => (
+                        <MenuItem value={index}>
+                          {alias}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
 
                   {/* <H3>Connected Ministers</H3>
                   {formatNamesList(partner.connected_ministers) || 'n/a'} */}
