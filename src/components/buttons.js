2044a144a3f8db793c847b133ae8755fa2fdc7fd
Matt Clark <clark.matthewr@gmail.com>
2021-11-12 20:53:48 -0600

    disable buttons when loading

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -9,7 +9,7 @@ import {
 export const CancelButton = (props) => {
   const { loading } = props;
   return (
-    <Button variant="contained" startIcon={<CancelIcon />} {...props}>
+    <Button disabled={loading} variant="contained" startIcon={<CancelIcon />} {...props}>
       {loading ? <CircularProgress size={25} color="inherit" /> : 'Cancel'}
     </Button>
   );
@@ -18,7 +18,7 @@ export const CancelButton = (props) => {
 export const DeleteButton = (props) => {
   const { loading } = props;
   return (
-    <Button color="secondary" variant="contained" startIcon={<DeleteIcon />} {...props}>
+    <Button disabled={loading} color="secondary" variant="contained" startIcon={<DeleteIcon />} {...props}>
       {loading ? <CircularProgress size={25} color="inherit" /> : 'Delete'}
     </Button>
   );
@@ -27,7 +27,7 @@ export const DeleteButton = (props) => {
 export const EditButton = (props) => {
   const { loading } = props;
   return (
-    <Button variant="contained" startIcon={<EditIcon />} {...props}>
+    <Button disabled={loading} variant="contained" startIcon={<EditIcon />} {...props}>
       {loading ? <CircularProgress size={25} color="inherit" /> : 'Edit'}
     </Button>
   );
@@ -36,7 +36,7 @@ export const EditButton = (props) => {
 export const SaveButton = (props) => {
   const { loading } = props;
   return (
-    <Button color="primary" variant="contained" startIcon={<SaveIcon />} {...props}>
+    <Button disabled={loading} color="primary" variant="contained" startIcon={<SaveIcon />} {...props}>
       {loading ? <CircularProgress size={25} color="inherit" /> : 'Save'}
     </Button>
   );
