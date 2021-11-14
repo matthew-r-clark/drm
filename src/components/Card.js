028de92382c0a14b3f9710850ba24d39849e909b
Matt Clark <clark.matthewr@gmail.com>
2021-11-13 18:58:20 -0600

    improve card view on mobile

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,28 @@ import CloseIcon from '@material-ui/icons/Close';
 
 import colors from 'styles/colors';
 
+const ModalBackground = styled(Modal)({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  overflow: 'scroll',
+});
+
+const Container = styled.div({
+  margin: 50,
+  maxWidth: '600px',
+  position: 'absolute',
+  backgroundColor: colors.white,
+  border: `1px solid ${colors.grayLight}`,
+  borderRadius: 10,
+  boxShadow: '1px 1px 2px 0px rgb(0 0 0 / 50%)',
+  padding: 25,
+  '@media (max-width: 500px)': {
+    top: 0,
+    margin: 0,
+  },
+});
+
 const CloseButton = styled(CloseIcon)({
   cursor: 'pointer',
   position: 'absolute',
@@ -17,26 +39,14 @@ const CloseButton = styled(CloseIcon)({
 
 export default function Card({ children, isOpen, close }) {
   return (
-    <Modal
+    <ModalBackground
       open={isOpen}
-      style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
       onClose={close}
     >
-      <div
-        style={{
-          margin: 50,
-          maxWidth: '600px',
-          position: 'absolute',
-          backgroundColor: colors.white,
-          border: `1px solid ${colors.grayLight}`,
-          borderRadius: 10,
-          boxShadow: '1px 1px 2px 0px rgb(0 0 0 / 50%)',
-          padding: 25,
-        }}
-      >
+      <Container>
         <CloseButton onClick={close} />
         {children}
-      </div>
-    </Modal>
+      </Container>
+    </ModalBackground>
   );
 }
